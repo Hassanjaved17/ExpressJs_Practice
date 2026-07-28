24200e9b83748700ed3d70514f2a02c7f99f1d1a
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-28 21:55:56 +0500

    practice expressjs

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 
 const app = express();
+const PORT = 3000;
 
 app.get('/',(req, res) =>{
     res.send("hello express");
 })
 
-app.listen(3000,() =>{
-    console.log("server is running on port 3000");
-})
+app.post('/home',(req, res) =>{
+    res.send("POST request to home");
+});
+
+
+app.listen(PORT,() =>{
+    console.log(`server is running on port ${PORT}`);
+})  
