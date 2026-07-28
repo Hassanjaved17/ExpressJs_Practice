0e375c031871a191ac4e72fe0fee5572588deb2f
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-28 22:01:44 +0500

    add POST route for home and organize route structure

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+// middleware
+app.use(express.json());
+
+// routes
 app.get('/',(req, res) =>{
     res.send("hello express");
 })
 
-app.post('/home',(req, res) =>{
-    res.send("POST request to home");
-});
+// nested routes
+app.get('/about',(req, res) =>{
+    res.send("About page");
+})
+
+// route with parameter
+app.get('/user/:id',(req, res) =>{
+    res.send(`User page for user with ID: ${req.params.id}`);
+})
 
 
 app.listen(PORT,() =>{
