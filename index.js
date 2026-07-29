e14de10e42b0d338e90bcfaca6cc2ad9d9dae3e8
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-29 12:28:21 +0500

    refactor: move express server setup to practice.js and clean up index.js

--- index.js
+++ index.js
@@ -2,31 +2,3 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
-
-// middleware
-app.use(express.json());
-
-// routes
-app.get('/',(req, res) =>{
-    res.send("hello express");
-})
-
-// nested routes
-app.get('/about',(req, res) =>{
-    res.send("About page");
-})
-
-// route with parameter
-app.get('/user/:id',(req, res) =>{
-    res.send(`User page for user with ID: ${req.params.id}`);
-})
-
-// route with query parameters
-app.get("/search", (req, res) => {
-  res.send(req.query);
-});
-
-// server
-app.listen(PORT,() =>{
-    console.log(`server is running on port ${PORT}`);
-})  
