10c5789bab1127e2e1a2edbf577f8aed913af823
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-29 22:45:10 +0500

    feat: set up express server with static files and EJS view engine

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
+app.get('/', (request, response) => {
+    response.render('index', { name: 'Hassan' });
+});
 
 
 
