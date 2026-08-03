5781287007236dc66112899308beec7a878bf97d
Hassanjaved17 <hassanaptech9@gmail.com>
2026-08-03 23:26:35 +0500

    fix: correct formatting and improve user authentication functions in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,22 @@
-const { createUser, findUser} = require("..models/user");
+const { createUser, findUser } = require("..models/user");
 
+exports.createUser = async (email, password) => { // Create a new user with the provided email and password
+    try {
+        await createUser(email, password); // Call the createUser function from the user model to create a new user
+    }
+    catch (err) { // Catch any errors that occur during the user creation process
+        throw err
+    }
+};
+
+exports.login = async (email, password) => { // Log in a user with the provided email and password  
+    try {
+        const user = await findUser(email); // Call the findUser function from the user model to find the user with the provided email
+        if (user.password === password) {  // Check if the provided password matches the user's password
+            return "Login successful";
+        }
+        return "Invalid email or password";
+    } catch (err) {  // Catch any errors that occur during the login process
+        throw err;
+    }
+};
