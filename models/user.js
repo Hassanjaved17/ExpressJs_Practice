9762705e3df19209f2312e980ce46ce5d6ba7b97
Hassanjaved17 <hassanaptech9@gmail.com>
2026-08-04 20:34:36 +0500

    feat: add user model with path and file system imports

---
+++ models/user.js
@@ -0,0 +1,6 @@
+const path = require('path'); // Import the 'path' module to work with file and directory paths
+
+const fs = require('fs'); // Import the 'fs' module to work with the file system
+
+const jsonFilePath = path.join(process.cwd(), 'data', 'users.json'); // Create a path to the 'users.json' file in the 'data' directory
+
