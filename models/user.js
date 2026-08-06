a941d5ad2a49a7777337390aa84c107fff441ed4
Hassanjaved17 <hassanaptech9@gmail.com>
2026-08-07 00:31:53 +0500

    fix: reset users.json to an empty array and add comment in user.js for clarity

--- models/user.js
+++ models/user.js
@@ -4,3 +4,4 @@ const fs = require('fs'); // Import the 'fs' module to work with the file system
 
 const jsonFilePath = path.join(process.cwd(), 'data', 'users.json'); // Create a path to the 'users.json' file in the 'data' directory
 
+//=========== READ FUNCTION ============== 
